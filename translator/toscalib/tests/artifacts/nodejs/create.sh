ae6585fe5ecb30185d606b0ac6d707e1cd742d76
Vahid Hashemian <vahidhashemian@us.ibm.com>
2015-05-14 15:24:14 -0700

    Installer scripts should not run simultaneously on the same server
    Due to Linux's limitation of apt-get and yum utilities that cannot
    run multiple sessions in parallel, SoftwareDeployment resources
    that install some package, are deployed to the same server, and do
    not have any dependency between them could fail installing those
    packages. We update the scripts to verify no other install session
    is in progress before attemting an install. This needs to be done
    in any future installer script that makes use of such utilities.
    
    Change-Id: Iea19fe27acffda7c54db801b728a66ea427e8662
    Closes-Bug: #1455251
    Closes-Bug: #1463595

--- translator/toscalib/tests/artifacts/nodejs/create.sh
+++ translator/toscalib/tests/artifacts/nodejs/create.sh
@@ -1,5 +1,21 @@
 #!/bin/sh -x
 # This script installs nodejs and the prereq
 add-apt-repository ppa:chris-lea/node.js
+
+#Trying to avoid multiple apt-get's running simultaneously (in the
+#rare occasion that the apt-get command fails rerun the script).
+while [[ "$(ps -A | grep apt-get | awk '{print $1}')" != "" ]]; do
+    echo "Waiting for the other apt-get process to complete ..."
+    r=$RANDOM && let "sec=$r/10000" && let "mil=($r%10000)/10"
+    sleep $sec.$mil
+done
 apt-get update
+
+#Trying to avoid multiple apt-get's running simultaneously (in the
+#rare occasion that the apt-get command fails rerun the script).
+while [[ "$(ps -A | grep apt-get | awk '{print $1}')" != "" ]]; do
+    echo "Waiting for the other apt-get process to complete ..."
+    r=$RANDOM && let "sec=$r/10000" && let "mil=($r%10000)/10"
+    sleep $sec.$mil
+done
 apt-get install -y nodejs build-essential
--- translator/toscalib/tests/artifacts/rsyslog/create.sh
+++ translator/toscalib/tests/artifacts/rsyslog/create.sh
@@ -1,6 +1,20 @@
 #!/bin/sh -x
 # This script installs rsyslog and the library for RELP
+
+#Trying to avoid multiple apt-get's running simultaneously (in the
+#rare occasion that the apt-get command fails rerun the script).
+while [[ "$(ps -A | grep apt-get | awk '{print $1}')" != "" ]]; do
+    echo "Waiting for the other apt-get process to complete ..."
+    r=$RANDOM && let "sec=$r/10000" && let "mil=($r%10000)/10"
+    sleep $sec.$mil
+done
 apt-get update
-apt-get install -y rsyslog
-apt-get update
-apt-get install -y rsyslog-relp
+
+#Trying to avoid multiple apt-get's running simultaneously (in the
+#rare occasion that the apt-get command fails rerun the script).
+while [[ "$(ps -A | grep apt-get | awk '{print $1}')" != "" ]]; do
+    echo "Waiting for the other apt-get process to complete ..."
+    r=$RANDOM && let "sec=$r/10000" && let "mil=($r%10000)/10"
+    sleep $sec.$mil
+done
+apt-get install -y rsyslog rsyslog-relp
