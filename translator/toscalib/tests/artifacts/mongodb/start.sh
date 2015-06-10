c251594ea6b8227b1be47ea96a47e5a6e1e229d6
Vahid Hashemian <vahidhashemian@us.ibm.com>
2015-06-10 12:18:18 -0700

    Prepare ELK TOSCA Template for Translation into HOT (2)
    Fix some further issues with the scripts for proper deployment of
    the translated HOT template.
    
    Partially Implements: blueprint translate-tosca-monitoring-usecase
    
    Change-Id: Ie8e357e61b0c6256c65774f64080df7ef6b9da46

--- translator/toscalib/tests/artifacts/mongodb/start.sh
+++ translator/toscalib/tests/artifacts/mongodb/start.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -x
 # This script starts mongodb
 /etc/init.d/mongodb stop
-rm /var/lib/mongod.lock
-mongod --dbpath /var/lib/mongodb &
+rm /var/lib/mongodb/mongod.lock
+mongod --repair
+mongod --dbpath /var/lib/mongodb --fork --logpath /var/log/mongod.log
