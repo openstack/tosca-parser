b43f595fd378a2e7f207f7b7b0cb811c58b1969a
Andreas Jaeger <aj@suse.com>
2020-05-30 17:11:27 +0200

    Switch to newer openstackdocstheme version
    Switch to openstackdocstheme 2.2.1 version. Using
    this version will allow especially:
    * Linking from HTML to PDF document
    * Allow parallel building of documents
    * Fix some rendering problems
    
    Update Sphinx version as well.
    
    Change pygments_style to 'native' since old theme version always used
    'native' and the theme now respects the setting and using 'sphinx' can
    lead to some strange rendering.
    
    Remove docs requirements from lower-constraints, they are not needed
    during install or test but only for docs building.
    
    openstackdocstheme renames some variables, so follow the renames
    before the next release removes them. A couple of variables are also
    not needed anymore, remove them.
    
    See also
    http://lists.openstack.org/pipermail/openstack-discuss/2020-May/014971.html
    
    Change-Id: Id214e9837a89e37ce1d26b7dad95682c53aff636

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -38,6 +38,10 @@ master_doc = 'index'
 project = u'tosca-parser'
 copyright = u'2013, OpenStack Foundation'
 
+# openstackdocstheme options
+openstackdocs_repo_name = 'openstack/tosca-parser'
+openstackdocs_use_storyboard = True
+
 # If true, '()' will be appended to :func: etc. cross-reference text.
 add_function_parentheses = True
 
@@ -46,7 +50,7 @@ add_function_parentheses = True
 add_module_names = True
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = 'native'
 
 # -- Options for HTML output --------------------------------------------------
 
