6f253c5a5ad8b5a9e607486269b86c8625ccd0b3
Manpreet Kaur <kaurmanpreet2620@gmail.com>
2021-01-30 13:41:44 +0530

    Dropping explicit unicode literal
    In python 3, all strings are considered as unicode string.
    
    This patch drops the explicit unicode literal (u'...')
    or (u"..") appearances from the unicode strings.
    
    Change-Id: Idd8fde4c4330c5125ed33f85d12fa6a551165f96

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -35,8 +35,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'tosca-parser'
-copyright = u'2013, OpenStack Foundation'
+project = 'tosca-parser'
+copyright = '2013, OpenStack Foundation'
 
 # openstackdocstheme options
 openstackdocs_repo_name = 'openstack/tosca-parser'
@@ -66,8 +66,8 @@ htmlhelp_basename = '%sdoc' % project
 latex_documents = [
     ('index',
      '%s.tex' % project,
-     u'%s Documentation' % project,
-     u'OpenStack Foundation', 'manual'),
+     '%s Documentation' % project,
+     'OpenStack Foundation', 'manual'),
 ]
 
 # Example configuration for intersphinx: refer to the Python standard library.
